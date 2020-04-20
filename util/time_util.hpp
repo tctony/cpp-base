451154242f12926bce72dd5e7e4a29ec71b19c52
Tony Tang <tangchang21@gmail.com>
2020-04-20 13:34:24 +0800

    add timeCostInMilliseconds

---
+++ util/time_util.hpp
@@ -0,0 +1,21 @@
+#if !defined(UTIL_TIME_UTIL_H)
+#define UTIL_TIME_UTIL_H
+
+#include <chrono>
+
+namespace base {
+namespace util {
+
+template <typename Func, typename... Args>
+inline uint64_t timeCostInMilliseconds(Func&& f, Args&&... args) {
+  auto start = std::chrono::steady_clock::now();
+  f(args...);
+  auto end = std::chrono::steady_clock::now();
+  return std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+      .count();
+}
+
+}  // namespace util
+}  // namespace base
+
+#endif  // UTIL_TIME_UTIL_H
---
+++ util/time_util_test.cpp
@@ -0,0 +1,15 @@
+#include "time_util.hpp"
+
+#include <gtest/gtest.h>
+
+#include <iostream>
+
+using base::util::timeCostInMilliseconds;
+
+TEST(time_util, timeCostInMilliseconds) {
+  auto cost = timeCostInMilliseconds([] { sleep(1); });
+  std::cout << cost << std::endl;
+
+  cost = timeCostInMilliseconds([](int t) { sleep(t); }, 1);
+  std::cout << cost << std::endl;
+}
