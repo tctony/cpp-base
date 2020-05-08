71d0a7552972b47944158c7fe56534c331591bb1
Tony Tang <tangchang21@gmail.com>
2020-05-08 13:47:35 +0800

    zlog_to_console include zlog.h and setup automaticly on include

--- zlog/zlog_to_console.h
+++ zlog/zlog_to_console.h
@@ -1,6 +1,14 @@
 #ifndef _ZLOG_TO_CONSOLE_H_
 #define _ZLOG_TO_CONSOLE_H_
 
+#include "zlog.h"
+
+namespace zlog {
+
+int zlogToConsole();
+
+}  // namespace zlog
+
 // clang-format off
 #define ZlogToConsoleSetup ___ZlogToConsoleSetup(__LINE__)
 #define ___ZlogToConsoleSetup(_line) ___ZlogToConsoleSetup_I(_line)
@@ -8,10 +16,6 @@
 static int __zlog_to_console_setup_##_line = zlog::zlogToConsole()
 // clang-formt on
 
-namespace zlog {
-
-int zlogToConsole();
-
-}  // namespace zlog
+ZlogToConsoleSetup;
 
 #endif
