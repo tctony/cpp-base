29be2e440e9e8d205455831e42fb907b416a7c27
Tony Tang <tangchang21@gmail.com>
2020-11-26 23:39:09 +0800

    add SimpleClock

--- util/time_util.hpp
+++ util/time_util.hpp
@@ -2,6 +2,7 @@
 #define UTIL_TIME_UTIL_H
 
 #include <chrono>
+#include <string>
 
 namespace base {
 namespace util {
@@ -32,6 +33,62 @@ inline uint64_t timeCostInNanoseconds(Func&& f, Args&&... args) {
   return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
       .count();
 }
+
+// a simple clock to count time interval
+class SimpleClock {
+ public:
+  SimpleClock(const std::string& name, bool autoStart = true)
+      : name_(name), state_(not_started) {
+    if (autoStart) {
+      start();
+    }
+  }
+
+  const std::string& name() { return name_; }
+
+  void start() {
+    assert(state_ == not_started && "can't start clock twice");
+    start_ = std::chrono::steady_clock::now();
+    state_ = running;
+  }
+
+  template <typename T = std::chrono::milliseconds>
+  uint64_t elapsed() {
+    assert(state_ == running &&
+           "can't get elapsed time interval when clock is not running");
+    return std::chrono::duration_cast<T>(std::chrono::steady_clock::now() -
+                                         start_)
+        .count();
+  }
+
+  template <typename T = std::chrono::milliseconds>
+  uint64_t end() {
+    assert(state_ == running &&
+           "can't not end clock when clock is not running");
+    end_ = std::chrono::steady_clock::now();
+    state_ = ended;
+    return cost<T>();
+  }
+
+  template <typename T = std::chrono::milliseconds>
+  uint64_t cost() {
+    assert(state_ == ended &&
+           "can't get cost time interval when clock is not ended");
+    return std::chrono::duration_cast<T>(std::chrono::steady_clock::now() -
+                                         start_)
+        .count();
+  }
+
+ private:
+  const std::string name_;
+  enum {
+    not_started = 0,
+    running,
+    ended,
+  } state_;
+  std::chrono::steady_clock::time_point start_;
+  std::chrono::steady_clock::time_point end_;
+};
 
 }  // namespace util
 }  // namespace base
--- util/time_util_test.cpp
+++ util/time_util_test.cpp
@@ -21,3 +21,18 @@ TEST(time_util, timeCostInMilliseconds) {
   cost = timeCostInNanoseconds([](int t) { sleep(t); }, 1);
   std::cout << cost << std::endl;
 }
+
+TEST(time_util, SimpleClock) {
+  auto clock = base::util::SimpleClock("simple_clock");
+  sleep(1);
+  std::cout << "seconds " << clock.elapsed<std::chrono::seconds>()
+            << " miliseconds " << clock.elapsed() << " microseconds "
+            << clock.elapsed<std::chrono::microseconds>() << " nanoseconds "
+            << clock.elapsed<std::chrono::nanoseconds>() << std::endl;
+  sleep(1);
+  auto cost = clock.end();
+  std::cout << "seconds " << clock.cost<std::chrono::seconds>()
+            << " miliseconds " << cost << " microseconds "
+            << clock.cost<std::chrono::microseconds>() << " nanoseconds "
+            << clock.cost<std::chrono::nanoseconds>() << std::endl;
+}
